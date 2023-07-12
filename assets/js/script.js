316ecf3ea3f34ddbc8a93e9cc9de89f2a646e495
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-12 06:56:00 -0600

    added starter javascript for handling API stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,9 @@ window.addEventListener('load', () => {
     let long;
     let lat;
 
+    // Check if the browser supports geolocation
     if (navigator.geolocation) {
+      // Get the current position of the user
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
@@ -28,3 +30,26 @@ window.addEventListener('load', () => {
             })
     }
 });
+
+// Function to get the weather data from the API
+searchBar.addEventListener('input', () => {
+    const location = searchBar.value;
+  
+    // Fetch the weather data from the API
+    // EDIT THE URL BELOW WITH OWN API KEY
+    const api = 'API_URL_HERE';
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(weatherData => {
+        const parsedWeatherData = JSON.parse(weatherData);
+
+        // Do something with the parsed weather data.
+    })
+    .catch(error => {
+        // Handle any errors that occur during the fetch request
+        console.log('Error:', error.message);
+    });    
+  });
